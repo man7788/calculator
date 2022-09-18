f1b60a6312b9c78f08203366e488b9d17dccbd14
man7788 <jerman1841@gmail.com>
2022-09-18 21:58:38 +0800

    Add basic math operators

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+ result = num1 + num2;
+ return result;
+}
+
+function subtract(baseNum, subtractNum) {
+  result = baseNum - subtractNum;
+  return result;
+ }
+
+ function multiply(num1, num2) {
+  result = num1 * num2;
+  return result;
+ }
+
+ function divide(dividend, divisor) {
+  result = dividend / divisor;
+  return result;
+ }
